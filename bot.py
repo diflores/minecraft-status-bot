65d8ad5a20067ea43dcabc8fa85c4f82414970d0
Daniela <diflores@uc.cl>
2019-12-27 15:34:27 -0300

    Add pucha command

--- bot.py
+++ bot.py
@@ -1,22 +1,20 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import CallbackContext
-from telegram import ParseMode
-import requests
 import os
-from mcstatus import MinecraftServer
+import socket
 
 from dotenv import load_dotenv
+from mcstatus import MinecraftServer
+from telegram import ParseMode
+from telegram.ext import CallbackContext, CommandHandler, Updater
+
 load_dotenv()
 
-MINECRAFT_SERVER_IP = os.getenv('MINECRAFT_SERVER_IP')
-MINECRAFT_SERVER_PORT = int(os.getenv('MINECRAFT_SERVER_PORT'))
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-NEW_PLAYERS_CHAT_ID = os.getenv('NEW_PLAYERS_CHAT_ID')
+MINECRAFT_SERVER_IP = os.getenv("MINECRAFT_SERVER_IP")
+MINECRAFT_SERVER_PORT = int(os.getenv("MINECRAFT_SERVER_PORT"))
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+NEW_PLAYERS_CHAT_ID = os.getenv("NEW_PLAYERS_CHAT_ID")
 
 minecraft_server = MinecraftServer(MINECRAFT_SERVER_IP, MINECRAFT_SERVER_PORT)
-updater = Updater(
-    token=TELEGRAM_BOT_TOKEN, use_context=True)
+updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 updater_jobs = updater.job_queue
 
@@ -26,8 +24,7 @@ def start(update, context):
         Replies to /start command.
     """
     print(f"Chat: {update.message.chat_id} started bot.")
-    context.bot.send_message(
-        chat_id=update.message.chat_id, text="¡Hola!")
+    context.bot.send_message(chat_id=update.message.chat_id, text="¡Hola!")
 
 
 def players_online(update, context):
@@ -35,41 +32,37 @@ def players_online(update, context):
         Replies to /players_online command.
     """
     print(f"Chat: {update.message.chat_id} requested players online.")
-    req = requests.get(f"https://api.mcsrvstat.us/2/{MINECRAFT_SERVER_IP}:{MINECRAFT_SERVER_PORT}")
-    response = req.json()
-    print(response)
-    if not response:
-        message = "Se produjo un error al consultar con la API. Intenta más tarde."
-    if response["players"]["online"] > 0:
-        players_online = response["players"]["list"]
+    query = minecraft_server.query()
+    current_players = query.players.names
+    if current_players:
+        players_online = current_players
         message = "Jugadores online:\n"
         for index, player in enumerate(players_online, 1):
             message += f"{index}. *{player}*\n"
     else:
         message = "No hay jugadores conectados."
     context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=message,
-        parse_mode=ParseMode.MARKDOWN)
+        chat_id=update.message.chat_id, text=message, parse_mode=ParseMode.MARKDOWN
+        )
+
 
 def server_status(update, context):
     """
         Replies to /server_status command.
     """
     print(f"Chat: {update.message.chat_id} requested server online.")
-    req = requests.get(f"https://api.mcsrvstat.us/2/{MINECRAFT_SERVER_IP}:{MINECRAFT_SERVER_PORT}")
-    response = req.json()
-    print(response)
-    if not response:
-        message = "Se produjo un error al consultar con la API. Intenta más tarde."
-    if response["online"]:
-        message = "El servidor está *online*."
-    else:
+    try:
+        status = minecraft_server.status()
+        if status:
+            message = "El servidor está *online*."
+        else:
+            message = "El servidor está *offline*."
+    except socket.timeout:
         message = "El servidor está *offline*."
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=message,
-        parse_mode=ParseMode.MARKDOWN)    
+    finally:
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=message, parse_mode=ParseMode.MARKDOWN
+            )
 
 
 def callback_new_player(context: CallbackContext):
@@ -91,22 +84,34 @@ def callback_new_player(context: CallbackContext):
             message = "Nuevos jugadores en línea:\n"
             for player in players_online:
                 message += f"- *{player}*\n"
-        context.bot.send_message(chat_id=NEW_PLAYERS_CHAT_ID,
-                                 text=message,
-                                 parse_mode=ParseMode.MARKDOWN)
+        context.bot.send_message(
+            chat_id=NEW_PLAYERS_CHAT_ID, text=message, parse_mode=ParseMode.MARKDOWN
+            )
     if current_players != old_players:
         with open(".players", "w") as f:  # Update players file if needed.
             for player in current_players:
                 f.write(f"{player}\n")
 
 
+def pucha(update, context):
+    """
+        Replies to /pucha command.
+    """
+    print(f"Chat: {update.message.chat_id} requested pucha GIF.")
+    context.bot.send_animation(
+        update.message.chat_id, "https://tenor.com/FzC5.gif",
+        )
+
+
 if __name__ == "__main__":
     start_handler = CommandHandler("start", start)
     players_online_handler = CommandHandler("players_online", players_online)
     server_status = CommandHandler("server_status", server_status)
+    pucha = CommandHandler("pucha", pucha)
     updater_jobs.run_repeating(callback_new_player, interval=60, first=0)
     print("Bot is running.")
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(players_online_handler)
     dispatcher.add_handler(server_status)
+    dispatcher.add_handler(pucha)
     updater.start_polling()
